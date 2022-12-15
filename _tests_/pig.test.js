48fbe7a9b07c84e86edd4d2b8ad80eaee10c2f52
RKrueg <thegckid@gmail.com>
2022-12-14 19:27:57 -0800

    test for creating a player object with userName and totalScore, test passed

--- _tests_/pig.test.js
+++ _tests_/pig.test.js
@@ -1,4 +1,4 @@
-import PigGame from "../src/pig";
+import {PigGame, Player} from "../src/pig";
 
 describe('PigGame', () => {
 
@@ -8,9 +8,26 @@ describe('PigGame', () => {
   });
 
   test('It should return a random number between 1 and 6', () => {
-    let number = PigGame.prototype.diceRoll();
+    let number = PigGame.prototype.randomNumber();
     expect(number).toBeGreaterThanOrEqual(1);
     expect(number).toBeLessThanOrEqual(6);
   })
+
+//   test('It should change currentScore to 0 if a 1 is rolled', () => {
+//     let game = new PigGame(0, 8);
+//     game.diceRoll();
+//     expect(game.currentScore).toEqual(0);
+//   })
+
+// })
+
+describe('Player', () => {
+
+test('It should create a Player object with userName and totalScore', () => {
+  let player1 = new Player("bob",0);
+  expect(player1.userName).toEqual('bob');
+  expect(player1.totalScore).toEqual(0)
+})
+})
 
 })
